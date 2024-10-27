a1d97b40772e55d85d292993b438ffa8a1c6dbd4
Sahil Gupta <155609546+2008sahil@users.noreply.github.com>
2024-10-27 17:59:45 +0530

    Update App.js

--- Backend/App.js
+++ Backend/App.js
@@ -7,10 +7,10 @@ app.use(express.json())
 app.use(cors())
 const dotenv = require("dotenv");
 // static files///////////////////
-app.use(express.static(path.join(__dirname,'./amazon-clone/build')))
+app.use(express.static(path.join(__dirname,'../amazon-clone/build')))
 
 app.get("*", (req,res)=>{
-        res.sendFile(path.join(__dirname,'./amazon-clone/build/index.html'))
+        res.sendFile(path.join(__dirname,'../amazon-clone/build/index.html'))
     })
     dotenv.config();
     const stripe=require('stripe')
